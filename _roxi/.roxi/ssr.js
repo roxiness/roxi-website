089c7725ccb64ed7a3edd055e6d349d63a58be4f
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-08-24 13:43:32 +0200

    add includeFiles

--- _roxi/.roxi/ssr.js
+++ _roxi/.roxi/ssr.js
@@ -1,10 +1,10 @@
 
 const fs = require('fs')
 const { ssr } = require('@sveltech/ssr')
-let script = '../../dist/build/bundle.js'
-let template = '../../dist/__app.html'
+let script = 'dist/build/bundle.js'
+let template = 'dist/__app.html'
 
-console.log('readdir', fs.readdirSync('../../'))
+console.log('readdir', fs.readdirSync('./'))
 
 // template = fs.existsSync(template) ? fs.readFileSync(template, 'utf8') : template
 // script = fs.existsSync(script) ? fs.readFileSync(script, 'utf8') : script
