af6b9aa894cae25e739b53e3612f5387c8858e49
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-08-24 13:16:06 +0200

    test

--- _roxi/.roxi/ssr.js
+++ _roxi/.roxi/ssr.js
@@ -1,7 +1,10 @@
 
+const fs = require('fs')
 const { ssr } = require('@sveltech/ssr')
 const template = 'dist/build/bundle.js'
 const script = 'dist/__app.html'
+
+console.log(fs.readdirSync('../../'))
 
 module.exports = async (req, res) => {
     const html = await ssr(template, script, req.url)
